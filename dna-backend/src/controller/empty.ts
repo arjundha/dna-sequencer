4e9c3d7302533daa938a006102b63aa77bd8c34c
Arjun <59514930+arjundha@users.noreply.github.com>
2023-12-17 17:01:59 -0800

    Empty file in controller for where we hold logic

---
+++ dna-backend/src/controller/empty.ts
@@ -0,0 +1,8 @@
+// TODO: Use strategy pattern to handle different types of sequencing requests
+// So we will need an interface for the strategy and a class for each strategy
+// Strategies are: DNA --> RNA
+//                 RNA --> Protein
+//                 DNA --> Protein
+//                 Protein --> DNA
+//                 Protein --> RNA
+//                 RNA --> DNA
