6ab2214a8e3b432c20032f3c557c02462e9746b2
Arjun <59514930+arjundha@users.noreply.github.com>
2024-03-11 16:53:04 -0700

    start

--- dna-backend/src/models/nucleic-acids/Codon.ts
+++ dna-backend/src/models/nucleic-acids/Codon.ts
@@ -1,3 +1,12 @@
+import {AminoAcid} from "../proteins/AminoAcid";
+
 export interface Codon {
 	baseTrio: string; // A trio of bases, e.g. ATG or AUG
+}
+
+export function codonToAminoAcid(codon: Codon): AminoAcid {
+	if (codon.baseTrio === "AUG") {
+		return {aminoAcid: "Met"};
+	}
+	return {aminoAcid: "Unknown"};
 }
