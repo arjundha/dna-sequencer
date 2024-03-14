4044339ebc25e3cf52ab07b49da3ed792e8b6198
Arjun <59514930+arjundha@users.noreply.github.com>
2024-03-13 22:56:43 -0700

    Started writing more tests for Codon

--- dna-backend/test/models/nucleic-acids/Codon.spec.ts
+++ dna-backend/test/models/nucleic-acids/Codon.spec.ts
@@ -1,29 +1,72 @@
-import {DNACodonToRNACodon, RNACodonToDNACodon} from "../../../src/models/nucleic-acids/Codon";
+import {
+	DNACodonToRNACodon,
+	RNACodonToDNACodon,
+	isIsoleucine,
+	isMethionine,
+	isStopCodon,
+} from "../../../src/models/nucleic-acids/Codon";
 
 describe("RNACodonToDNACodon", () => {
-	test("changes codon with U to use T instead", () => {
+	it("changes codon with U to use T instead", () => {
 		expect(RNACodonToDNACodon({baseTrio: "AUG"})).toEqual({baseTrio: "ATG"});
 	});
 
-	test("maintains codons that do not contain U", () => {
+	it("maintains codons that do not contain U", () => {
 		expect(RNACodonToDNACodon({baseTrio: "AAG"})).toEqual({baseTrio: "AAG"});
 	});
 
-	test("converts codons that are entirely comprised of U", () => {
+	it("converts codons that are entirely comprised of U", () => {
 		expect(RNACodonToDNACodon({baseTrio: "UUU"})).toEqual({baseTrio: "TTT"});
 	});
 });
 
 describe("DNACodonToRNACodon", () => {
-	test("changes codon with T to use U instead", () => {
+	it("changes codon with T to use U instead", () => {
 		expect(DNACodonToRNACodon({baseTrio: "ATG"})).toEqual({baseTrio: "AUG"});
 	});
 
-	test("maintains codons that do not contain T", () => {
+	it("maintains codons that do not contain T", () => {
 		expect(DNACodonToRNACodon({baseTrio: "AAG"})).toEqual({baseTrio: "AAG"});
 	});
 
-	test("converts codons that are entirely comprised of T", () => {
+	it("converts codons that are entirely comprised of T", () => {
 		expect(DNACodonToRNACodon({baseTrio: "TTT"})).toEqual({baseTrio: "UUU"});
+	});
+});
+
+describe("isMethionine", () => {
+	it("is true when AUG", () => {
+		expect(isMethionine("AUG")).toBe(true);
+	});
+
+	it("is false when ATG", () => {
+		expect(isMethionine("ATG")).toBe(false);
+	});
+
+	it("is false when something else", () => {
+		expect(isMethionine("UUU")).toBe(false);
+	});
+});
+
+describe("isStopCodon", () => {
+	test.each([
+		["UAA", true],
+		["UAG", true],
+		["UGA", true],
+		["AAA", false],
+	])("with %s it should be %s", (input, expected) => {
+		expect(isStopCodon(input)).toBe(expected);
+	});
+});
+
+describe("isIsoleucine", () => {
+	test.each([
+		["AUU", true],
+		["AUC", true],
+		["AUA", true],
+		["AUG", false], // Methionine
+		["AAA", false], // Lysine
+	])("with %s it should be %s", (input, expected) => {
+		expect(isIsoleucine(input)).toBe(expected);
 	});
 });
