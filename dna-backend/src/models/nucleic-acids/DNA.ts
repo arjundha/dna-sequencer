0765302f088f674ca9b89603257064fa429be177
Arjun <59514930+arjundha@users.noreply.github.com>
2024-03-25 19:49:56 -0700

    Completed DNA helpers

--- dna-backend/src/models/nucleic-acids/DNA.ts
+++ dna-backend/src/models/nucleic-acids/DNA.ts
@@ -1,6 +1,110 @@
 import {Base} from "./Base";
 import {Codon} from "./Codon";
+import {RNA} from "./RNA";
 
 export interface DNA {
 	nucleotides: Codon[]; // A list of codons ex. [ { baseTrio: "ATG" }, { baseTrio: "GCT" }]
+}
+
+/**
+ * Checks if a string is a valid DNA string (only contains A, T, C, or G)
+ *
+ * @param dna The string to check (CASE SENSTIVE)
+ * @returns A boolean indicating if the string is a valid DNA string
+ */
+export function isStringDNA(dna: string): boolean {
+	return /^[ATCG]+$/.test(dna);
+}
+
+/**
+ * Transforms a string into a DNA object
+ *
+ * @param dna The string being transformed
+ * @returns A DNA object that represents the original string
+ */
+export function stringToDNA(dna: string): DNA {
+	let codons = [];
+	// Find start (try to start at a start codon)
+	let start = dna.indexOf("ATG");
+
+	// If no start codon, start at beginning
+	if (start === -1) {
+		start = 0;
+	}
+
+	for (let i = start; i < dna.length; i += 3) {
+		// Make sure we are not going out of bounds
+		if (i + 3 > dna.length) {
+			break;
+		}
+		const codon = dna.slice(i, i + 3);
+		codons.push({baseTrio: codon});
+	}
+	return {nucleotides: codons};
+}
+
+/**
+ * Checks if a DNA object is valid
+ *
+ * @param dna The DNA object being validated
+ * @returns A boolean indicating if the DNA object is valid
+ */
+export function isDNA(dna: DNA): boolean {
+	return dna.nucleotides.every((codon) => isStringDNA(codon.baseTrio));
+}
+
+/**
+ * Checks if a DNA object contains a start codon
+ *
+ * @param dna The DNA object being checked
+ * @returns A boolean indicating if the DNA object contains a start codon
+ */
+export function containsStartCodon(dna: DNA): boolean {
+	return dna.nucleotides.some((codon) => codon.baseTrio === "ATG");
+}
+
+/**
+ * Checks if a DNA object contains a stop codon
+ *
+ * @param dna The DNA object being checked
+ * @returns A boolean indicating if the DNA object contains a stop codon
+ */
+export function containsStopCodon(dna: DNA): boolean {
+	return dna.nucleotides.some(
+		(codon) => codon.baseTrio === "TAA" || codon.baseTrio === "TAG" || codon.baseTrio === "TGA",
+	);
+}
+
+/**
+ * Checks if a DNA object contains a stop codon after a start codon
+ *
+ * @param dna The DNA object being checked
+ * @returns A boolean indicating if the DNA object contains a stop codon that occurs after a start codon
+ */
+export function stopCodonAfterStartCodon(dna: DNA): boolean {
+	const startCodonIndex = dna.nucleotides.findIndex((codon) => codon.baseTrio === "ATG");
+	console.log(startCodonIndex);
+
+	if (startCodonIndex === -1) {
+		// if there is no start codon, then we fail
+		return false;
+	}
+
+	const stopCodonIndex = dna.nucleotides.findIndex(
+		(codon) => codon.baseTrio === "TAA" || codon.baseTrio === "TAG" || codon.baseTrio === "TGA",
+	);
+	return startCodonIndex < stopCodonIndex;
+}
+
+/**
+ * Converts a DNA object into an RNA object
+ *
+ * @param dna The DNA object being translated
+ * @returns An RNA object that represents the original DNA object
+ */
+export function DNAtoRNA(dna: DNA): RNA {
+	const rna = dna.nucleotides.map((codon) => {
+		return {baseTrio: codon.baseTrio.replace(/T/g, "U")};
+	});
+	return {ribonucleotides: rna};
 }
--- dna-backend/test/models/nucleic-acids/DNA.spec.ts
+++ dna-backend/test/models/nucleic-acids/DNA.spec.ts
@@ -1,5 +1,136 @@
-describe("stub", () => {
-	test("stub", () => {
-		expect(true).toBe(true);
+import {
+	containsStartCodon,
+	containsStopCodon,
+	isStringDNA,
+	stopCodonAfterStartCodon,
+	stringToDNA,
+} from "../../../src/models/nucleic-acids/DNA";
+
+const dnaStartAndStop = {nucleotides: [{baseTrio: "ATG"}, {baseTrio: "TAA"}]};
+const dnaStartNoStop = {nucleotides: [{baseTrio: "ATG"}, {baseTrio: "AAA"}]};
+const dnaNoStart = {nucleotides: [{baseTrio: "AAA"}, {baseTrio: "TAA"}]};
+const dnaNoStartOrStop = {nucleotides: [{baseTrio: "AAA"}, {baseTrio: "AAA"}]};
+
+describe("isStringDNA", () => {
+	it("passes with well-formed string", () => {
+		expect(isStringDNA("ATGAAACCCTTTGGG")).toBeTruthy();
+	});
+
+	it("passes when string is all same letter", () => {
+		expect(isStringDNA("AAAAAAAAAA")).toBeTruthy();
+	});
+
+	it("passes when string is one valid letter", () => {
+		expect(isStringDNA("A")).toBeTruthy();
+	});
+
+	it("fails when string is empty", () => {
+		expect(isStringDNA("")).toBeFalsy();
+	});
+
+	it("fails when string is one invalid letter", () => {
+		expect(isStringDNA("U")).toBeFalsy();
+	});
+
+	it("fails when string is lowercase valid letters", () => {
+		expect(isStringDNA("aaaccctttggg")).toBeFalsy();
+	});
+
+	it("fails when string is lowercase and uppercase valid letters", () => {
+		expect(isStringDNA("aaaCCCtttGGG")).toBeFalsy();
+	});
+
+	it("fails when string is valid letters with one invalid", () => {
+		expect(isStringDNA("ATCCGGAUG")).toBeFalsy();
+	});
+});
+
+describe("stringToDNA", () => {
+	it("should create an empty DNA object if the string is empty", () => {
+		expect(stringToDNA("")).toEqual({nucleotides: []});
+	});
+
+	it("should create a well formed DNA object", () => {
+		expect(stringToDNA("ATCGCCAAT")).toEqual({nucleotides: [{baseTrio: "ATC"}, {baseTrio: "GCC"}, {baseTrio: "AAT"}]});
+	});
+
+	it("should start at ATG if there is one and create a well formed DNA object", () => {
+		expect(stringToDNA("ATCGCCATGAAT")).toEqual({nucleotides: [{baseTrio: "ATG"}, {baseTrio: "AAT"}]});
+	});
+
+	it("should start at first ATG if there are two", () => {
+		expect(stringToDNA("ATATGCGCCATATGGAAT")).toEqual({
+			nucleotides: [{baseTrio: "ATG"}, {baseTrio: "CGC"}, {baseTrio: "CAT"}, {baseTrio: "ATG"}, {baseTrio: "GAA"}],
+		});
+	});
+
+	it("should ignore extra bases if there are not perfect groups of 3", () => {
+		expect(stringToDNA("AAAT")).toEqual({nucleotides: [{baseTrio: "AAA"}]});
+	});
+});
+
+describe("stopCodonAfterStartCodon", () => {
+	it("passes when there is a stop codon after a start codon", () => {
+		expect(stopCodonAfterStartCodon({nucleotides: [{baseTrio: "ATG"}, {baseTrio: "TAA"}]})).toBeTruthy();
+	});
+
+	it("passes when there is a stop codon after a start codon (multiple starts)", () => {
+		expect(
+			stopCodonAfterStartCodon({
+				nucleotides: [{baseTrio: "ATG"}, {baseTrio: "ATG"}, {baseTrio: "ATG"}, {baseTrio: "TAA"}],
+			}),
+		).toBeTruthy();
+	});
+
+	it("fails when nucleotides are empty", () => {
+		expect(stopCodonAfterStartCodon({nucleotides: []})).toBeFalsy();
+	});
+
+	it("fails when no start codon", () => {
+		expect(stopCodonAfterStartCodon({nucleotides: [{baseTrio: "TAA"}, {baseTrio: "TAA"}]})).toBeFalsy();
+	});
+
+	it("fails when no stop codon", () => {
+		expect(stopCodonAfterStartCodon({nucleotides: [{baseTrio: "ATG"}, {baseTrio: "AAA"}]})).toBeFalsy();
+	});
+
+	it("fails when stop is before start", () => {
+		expect(stopCodonAfterStartCodon({nucleotides: [{baseTrio: "TAA"}, {baseTrio: "ATG"}]})).toBeFalsy();
+	});
+});
+
+describe("containsStartCodon", () => {
+	it("passes when there is a start codon and a stop", () => {
+		expect(containsStartCodon(dnaStartAndStop)).toBeTruthy();
+	});
+
+	it("passes when there is a start codon and no stop", () => {
+		expect(containsStartCodon(dnaStartNoStop)).toBeTruthy();
+	});
+
+	it("should fail when there is no start codon and a stop", () => {
+		expect(containsStartCodon(dnaNoStart)).toBeFalsy();
+	});
+
+	it("should fail when there is no start codon and no stop", () => {
+		expect(containsStartCodon(dnaNoStartOrStop)).toBeFalsy();
+	});
+});
+
+describe("containsStopCodon", () => {
+	it("passes when there is a start codon and a stop", () => {
+		expect(containsStopCodon(dnaStartAndStop)).toBeTruthy();
+	});
+
+	it("should fail when there is a start codon and no stop", () => {
+		expect(containsStopCodon(dnaStartNoStop)).toBeFalsy();
+	});
+
+	it("should pass when there is no start codon and a stop", () => {
+		expect(containsStopCodon(dnaNoStart)).toBeTruthy();
+	});
+
+	it("should fail when there is no start codon and no stop", () => {
+		expect(containsStopCodon(dnaNoStartOrStop)).toBeFalsy();
 	});
 });
