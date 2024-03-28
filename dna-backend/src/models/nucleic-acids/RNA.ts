00fdd58883fe232aa50dd2e812acf9c58d46adab
Arjun <59514930+arjundha@users.noreply.github.com>
2024-03-27 22:22:47 -0700

    RNA helpers

--- dna-backend/src/models/nucleic-acids/RNA.ts
+++ dna-backend/src/models/nucleic-acids/RNA.ts
@@ -1,6 +1,16 @@
 import {Base} from "./Base";
-import {Codon} from "./Codon";
+import {Codon, DNACodonToRNACodon} from "./Codon";
+import {DNA} from "./DNA";
 
 export interface RNA {
 	ribonucleotides: Codon[]; // A list of codons ex. [ { baseTrio: "AUG" }, { baseTrio: "GCU" }]
+}
+
+export function convertDNAtoRNA(dna: DNA): RNA {
+	let codons = [];
+	for (let i = 0; i < dna.nucleotides.length; i++) {
+		const codon = dna.nucleotides[i];
+		codons.push(DNACodonToRNACodon(codon));
+	}
+	return {ribonucleotides: codons};
 }
--- dna-backend/test/models/nucleic-acids/RNA.spec.ts
+++ dna-backend/test/models/nucleic-acids/RNA.spec.ts
@@ -1,5 +1,29 @@
-describe("stub", () => {
-	test("stub", () => {
-		expect(true).toBe(true);
+import {convertDNAtoRNA} from "../../../src/models/nucleic-acids/RNA";
+
+describe("convertDNAtoRNA", () => {
+	test("it should be idempotent if there are no Thymine groups", () => {
+		expect(convertDNAtoRNA({nucleotides: [{baseTrio: "AAA"}]})).toEqual({ribonucleotides: [{baseTrio: "AAA"}]});
+	});
+
+	test("it should not break if codons are empty", () => {
+		expect(convertDNAtoRNA({nucleotides: []})).toEqual({ribonucleotides: []});
+	});
+
+	test("it should convert when only one codon", () => {
+		expect(convertDNAtoRNA({nucleotides: [{baseTrio: "ATC"}]})).toEqual({ribonucleotides: [{baseTrio: "AUC"}]});
+	});
+
+	test("it should convert when only two codons", () => {
+		expect(convertDNAtoRNA({nucleotides: [{baseTrio: "ATC"}, {baseTrio: "CTT"}]})).toEqual({
+			ribonucleotides: [{baseTrio: "AUC"}, {baseTrio: "CUU"}],
+		});
+	});
+
+	test("it should convert when many codons", () => {
+		expect(
+			convertDNAtoRNA({nucleotides: [{baseTrio: "ATC"}, {baseTrio: "CTT"}, {baseTrio: "TTT"}, {baseTrio: "GGA"}]}),
+		).toEqual({
+			ribonucleotides: [{baseTrio: "AUC"}, {baseTrio: "CUU"}, {baseTrio: "UUU"}, {baseTrio: "GGA"}],
+		});
 	});
 });
