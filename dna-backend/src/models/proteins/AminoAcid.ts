e6f97bd277a3ef43fd9a17fd9ad9a5115987543d
Arjun <59514930+arjundha@users.noreply.github.com>
2024-03-27 23:20:13 -0700

    Functions for formatting aa strings

--- dna-backend/src/models/proteins/AminoAcid.ts
+++ dna-backend/src/models/proteins/AminoAcid.ts
@@ -4,3 +4,19 @@ export interface AminoAcid {
 	isStartCodon?: boolean; // Whether or not this amino acid is a start codon
 	isStopCodon?: boolean; // Whether or not this amino acid is a stop codon
 }
+
+export function printAminoAcid(aminoAcid: AminoAcid): string {
+	return `${aminoAcid.aminoAcid} (${aminoAcid.fullName})`;
+}
+
+export function printShortAminoAcid(aminoAcid: AminoAcid): string {
+	return aminoAcid.aminoAcid;
+}
+
+export function printLongAminoAcid(aminoAcid: AminoAcid): string {
+	return aminoAcid.fullName;
+}
+
+export function printAminoAcids(aminoAcids: AminoAcid[]): string {
+	return aminoAcids.map(printAminoAcid).join(", ");
+}
