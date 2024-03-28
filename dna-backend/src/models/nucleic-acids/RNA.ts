a9e0ea6064f6baa19b547a91537f995b1367a4d1
Arjun <59514930+arjundha@users.noreply.github.com>
2024-03-27 23:19:59 -0700

    Added function doc

--- dna-backend/src/models/nucleic-acids/RNA.ts
+++ dna-backend/src/models/nucleic-acids/RNA.ts
@@ -6,6 +6,12 @@ export interface RNA {
 	ribonucleotides: Codon[]; // A list of codons ex. [ { baseTrio: "AUG" }, { baseTrio: "GCU" }]
 }
 
+/**
+ * Converts a DNA object to an RNA object
+ *
+ * @param dna The DNA object being translated to RNA
+ * @returns The RNA object translated from DNA
+ */
 export function convertDNAtoRNA(dna: DNA): RNA {
 	let codons = [];
 	for (let i = 0; i < dna.nucleotides.length; i++) {
