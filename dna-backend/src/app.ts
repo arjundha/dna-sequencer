5a935ad5e120c4b374c5da35f6cf80f632bb311a
Arjun <59514930+arjundha@users.noreply.github.com>
2024-04-03 22:50:30 -0700

    Sanity check for endpoints

--- dna-backend/src/app.ts
+++ dna-backend/src/app.ts
@@ -3,6 +3,10 @@ import dotenv from "dotenv";
 import cors from "cors";
 import helmet from "helmet";
 
+import fsextra from "fs-extra";
+import multer from "multer";
+import {translateDNAStringtoProtein} from "./controller/dna-convertor";
+
 // Env
 dotenv.config();
 
@@ -14,6 +18,8 @@ const port = process.env.PORT || 8000;
 app.use(cors());
 app.use(helmet());
 
+const upload = multer({dest: "uploads/"});
+
 // Routes / Endpoints
 app.get("/", (req: Request, res: Response) => {
 	// Either static serve here, or make frontend and backend link with separate ports
@@ -21,6 +27,36 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.get("/test", (req: Request, res: Response) => {
+	res.send("Test");
+});
+
+app.post("/translateDNA", upload.none(), async (req: Request, res: Response) => {
+	try {
+		console.log("/translateDNA called");
+		console.log(req.body);
+		console.log(req.body.text); // TODO: update this with front end form input name
+		const dnaString = req.body.text;
+		console.log(dnaString);
+		const response = await translateDNAStringtoProtein(dnaString);
+		console.log(response);
+		res.status(200).json(response);
+	} catch (error) {
+		console.log(error);
+		res.status(400).json({error: error});
+	}
+});
+
+app.post("/upload", upload.single("text-file"), (req: Request, res: Response) => {
+	console.log(req.file);
+	// console.log(req);
+	if (!req.file) {
+		res.send("No file uploaded");
+		return;
+	}
+	// const multerText = Buffer.from(req.file.buffer).toString("utf-8"); // this reads and converts the contents of the text file into string
+	// console.log(multerText);
+	let data = fsextra.readFileSync(req.file.path, "utf8");
+	console.log(data);
 	res.send("Test");
 });
 
