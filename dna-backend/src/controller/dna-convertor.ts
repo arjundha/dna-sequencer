3bfe0fb5fd592f996e32ae497b5039ecdf0aabd1
Arjun <59514930+arjundha@users.noreply.github.com>
2024-04-09 22:34:55 -0700

    Final backend functions

--- dna-backend/src/controller/dna-convertor.ts
+++ dna-backend/src/controller/dna-convertor.ts
@@ -1,16 +1,21 @@
-import {DNA, stringToDNA} from "../models/nucleic-acids/DNA";
-import {RNA, convertDNAtoRNA} from "../models/nucleic-acids/RNA";
-import {Polypeptide, convertRNAToFullPolypeptide} from "../models/proteins/Polypeptide";
-import {stringDNAtoPolypeptide, stringRNAtoPolypeptide} from "./performers/string-performers";
+import {Polypeptide} from "../models/proteins/Polypeptide";
+import {sanitizeNucleotideString, stringDNAtoPolypeptide, stringRNAtoPolypeptide} from "./performers/string-performers";
 import {validateDNAString, validateRNAString} from "./validators/dna-validator";
 
+/**
+ * Converts a DNA string into a list of polypeptides that it codes for
+ *
+ * @param dnaString The DNA object being translated to a list of polypeptides
+ * @returns A list of any and all possible polypeptides that the DNA codes for
+ *
+ * @throws Error if the DNA string is empty or invalid
+ */
 export function translateDNAStringtoProtein(dnaString: string): Promise<Polypeptide[]> {
 	if (!dnaString) {
 		return Promise.reject(new Error("DNA sequence is empty."));
 	}
 	try {
-		dnaString = dnaString.toUpperCase(); // Make all characters uppercase
-		dnaString = dnaString.replace(/\s/g, ""); // Remove all whitespace
+		dnaString = sanitizeNucleotideString(dnaString);
 		validateDNAString(dnaString);
 	} catch (error) {
 		return Promise.reject(error);
@@ -18,14 +23,19 @@ export function translateDNAStringtoProtein(dnaString: string): Promise<Polypept
 	return stringDNAtoPolypeptide(dnaString);
 }
 
+/**
+ * Converts RNA strings into a list of polypeptides that they code for
+ *
+ * @param rnaString The RNA string being translated to a list of polypeptides
+ * @returns A list of any and all possible polypeptides that the RNA codes for
+ */
 export function translateRNAStringtoProtein(rnaString: string): Promise<Polypeptide[]> {
 	// validate the input
 	if (!rnaString) {
 		return Promise.reject(new Error("RNA sequence is empty."));
 	}
 	try {
-		rnaString = rnaString.toUpperCase(); // Make all characters uppercase
-		rnaString = rnaString.replace(/\s/g, ""); // Remove all whitespace
+		rnaString = sanitizeNucleotideString(rnaString);
 		validateRNAString(rnaString);
 	} catch (error) {
 		return Promise.reject(error);
--- dna-backend/src/controller/performers/string-performers.ts
+++ dna-backend/src/controller/performers/string-performers.ts
@@ -2,6 +2,28 @@ import {DNA, stringToDNA} from "../../models/nucleic-acids/DNA";
 import {RNA, convertDNAtoRNA} from "../../models/nucleic-acids/RNA";
 import {Polypeptide, convertRNAToFullPolypeptide, sequencePolypeptides} from "../../models/proteins/Polypeptide";
 
+/**
+ * Reformats a string of nucleotides to be all uppercase and have no whitespace
+ *
+ * @param codons String of nucleotides to sanitize (DNA OR RNA)
+ * @returns The sanitized string
+ */
+export function sanitizeNucleotideString(codons: string): string {
+	try {
+		codons = codons.toUpperCase(); // Make all characters uppercase
+		codons = codons.replace(/\s/g, ""); // Remove all whitespace
+		return codons;
+	} catch (error) {
+		throw new Error("Error sanitizing DNA string");
+	}
+}
+
+/**
+ * Converts a DNA string into a list of polypeptides that it codes for
+ *
+ * @param dnaString The DNA object being translated to a list of polypeptides
+ * @returns A list of any and all possible polypeptides that the DNA codes for
+ */
 export async function stringDNAtoPolypeptide(dnaString: string): Promise<Polypeptide[]> {
 	try {
 		let dna: DNA = stringToDNA(dnaString);
@@ -14,6 +36,12 @@ export async function stringDNAtoPolypeptide(dnaString: string): Promise<Polypep
 	}
 }
 
+/**
+ * Converts RNA strings into a list of polypeptides that they code for
+ *
+ * @param rnaString The RNA string being translated to a list of polypeptides
+ * @returns A list of any and all possible polypeptides that the RNA codes for
+ */
 export async function stringRNAtoPolypeptide(rnaString: string): Promise<Polypeptide[]> {
 	try {
 		// Easiest way is to turn the RNA into DNA and then convert it to a polypeptide
