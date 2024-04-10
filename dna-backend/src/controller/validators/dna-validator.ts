a24b8a3a035fb50a7ab6b9e217560f4577861c3c
Arjun <59514930+arjundha@users.noreply.github.com>
2024-04-09 22:32:05 -0700

    Functions to validate strings

--- dna-backend/src/controller/validators/dna-validator.ts
+++ dna-backend/src/controller/validators/dna-validator.ts
@@ -1,6 +1,12 @@
 import {isStringDNA} from "../../models/nucleic-acids/DNA";
 import {isStringRNA} from "../../models/nucleic-acids/RNA";
 
+/**
+ * Validates that a string is a valid DNA string (only contains A, T, C, or G)
+ *
+ * @param dna The DNA string to check (CASE SENSTIVE)
+ * @returns A boolean indicating if the string is a valid DNA string
+ */
 export function validateDNAString(dna: string): boolean {
 	if (isStringDNA(dna)) {
 		return true;
@@ -9,6 +15,12 @@ export function validateDNAString(dna: string): boolean {
 	}
 }
 
+/**
+ * Validates that a string is a valid RNA string (only contains A, U, C, or G)
+ *
+ * @param rna The RNA string to check (CASE SENSTIVE)
+ * @returns A boolean indicating if the string is a valid RNA string
+ */
 export function validateRNAString(rna: string): boolean {
 	if (isStringRNA(rna)) {
 		return true;
