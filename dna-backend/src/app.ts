99398c62ea57f702d418dcb9004137c167732ca5
Arjun <59514930+arjundha@users.noreply.github.com>
2024-06-11 21:47:23 -0700

    added body parser

--- dna-backend/src/app.ts
+++ dna-backend/src/app.ts
@@ -1,24 +1,23 @@
 import express, {Express, Request, Response, Application} from "express";
 import dotenv from "dotenv";
-import router from "./routes"
+import router from "./routes";
 import cors from "cors";
 import helmet from "helmet";
-
+import bodyParser from "body-parser";
 
 // Env
 dotenv.config();
 
 // Server
 const app: Application = express();
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 // Middleware
 app.use(cors());
 app.use(helmet());
 
-
 // Routes / Endpoints
-app.use('/', router);
-
+app.use("/", router);
 
 export default app;
