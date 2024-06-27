bf8f64abadd5d33d4f90b941f5fdf73609dc6cc7
Arjun <59514930+arjundha@users.noreply.github.com>
2024-06-27 14:05:18 -0700

    Used said theme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import {ThemeProvider} from "@mui/material/styles";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import theme from "./themes/AppTheme";
+import "./index.css";
+import CssBaseline from "@mui/material/CssBaseline";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<App />
+		</ThemeProvider>
+	</React.StrictMode>,
+);
