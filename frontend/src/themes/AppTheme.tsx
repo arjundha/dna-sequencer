60c59dd0e48a8667501908271b70e1f3e2fa401e
Arjun <59514930+arjundha@users.noreply.github.com>
2024-06-27 18:53:15 -0700

    Updated the theme

--- frontend/src/themes/AppTheme.tsx
+++ frontend/src/themes/AppTheme.tsx
@@ -21,10 +21,10 @@ export const themeOptions: ThemeOptions = {
 		MuiButton: {
 			styleOverrides: {
 				root: {
-					background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
+					background: "linear-gradient(45deg, #083D77 30%, #F95738 90%)",
 					border: 0,
 					borderRadius: 3,
-					boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
+					boxShadow: "0 3px 5px 2px rgba(8, 61, 119, .3)",
 					color: "white",
 					height: 48,
 					padding: "0 30px",
