e3b3fe0c1a3965b926f37703fd105c4791a809d8
Arjun <59514930+arjundha@users.noreply.github.com>
2024-06-28 17:50:24 -0700

    Finished form

--- frontend/src/components/SequencerForm.tsx
+++ frontend/src/components/SequencerForm.tsx
@@ -1,10 +1,22 @@
-import {Box, Button, TextField} from "@mui/material";
+import {Box, Button, FormControlLabel, Radio, RadioGroup, TextField} from "@mui/material";
 import {useState} from "react";
+import {Polypeptide} from "../interfaces/Polypeptide";
+import Confetti from "react-confetti";
 
 const SequencerForm = () => {
+	// Hooks
 	const [isFormInvalid, setIsFormInvalid] = useState(false);
 	const [errorMessage, setErrorMessage] = useState("");
+	const [sequencedProtein, setSequencedProtein] = useState("");
+	const [sequenceSuccess, setSequenceSuccess] = useState(false);
+	const [selectedValue, setSelectedValue] = useState("short");
 
+	// Global variables
+	let hasBeenFocused = false;
+	let aminoAcids: string = "";
+	let fullNames: string = "";
+
+	// Helpers
 	function isValidSequence(sequence: string): boolean {
 		const regex = /^[ATCG]*$/;
 		return regex.test(sequence);
@@ -30,13 +42,24 @@ const SequencerForm = () => {
 
 	// Submit Handler for Button!
 	const submitHandler = (event: React.SyntheticEvent) => {
-		console.log("submit called");
 		event.preventDefault();
 		let data: FormData = new FormData(event.target as HTMLFormElement);
 		let text: string = data.get("text") as string;
 		if (validateInput(text)) {
 			translateDNAStringtoProtein(text);
 		}
+	};
+
+	// Focus Handler for TextField!
+	const focusHandler = (event: React.FocusEvent<HTMLInputElement | HTMLTextAreaElement, Element>) => {
+		if (!hasBeenFocused) {
+			event.target.select();
+			hasBeenFocused = true;
+		}
+	};
+
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setSelectedValue(event.target.value);
 	};
 
 	// Function to call the backend API and receive some response
@@ -57,6 +80,9 @@ const SequencerForm = () => {
 						console.log(data);
 						// @TODO do something here with the data
 						// Also do the cool UX idea
+						generatePeptideString(data);
+						selectedValue === "short" ? setSequencedProtein(aminoAcids) : setSequencedProtein(fullNames);
+						generateConfetti();
 					});
 				} else {
 					// If the response is NOT ok then do the following and catch the error
@@ -70,24 +96,85 @@ const SequencerForm = () => {
 			});
 	}
 
+	function generatePeptideString(polypeptides: Polypeptide[]) {
+		let tempAminoAcids: string = "";
+		let tempFullNames: string = "";
+		for (let i = 0; i < polypeptides.length; i++) {
+			for (let j = 0; j < polypeptides[i].aminoAcids.length; j++) {
+				if (j !== 0) {
+					tempAminoAcids += ", ";
+					tempFullNames += ", ";
+				}
+				tempAminoAcids += polypeptides[i].aminoAcids[j].aminoAcid;
+				tempFullNames += polypeptides[i].aminoAcids[j].fullName;
+			}
+			// New line for each polypeptide
+			tempAminoAcids += "\n";
+			tempFullNames += "\n";
+		}
+		aminoAcids = tempAminoAcids;
+		fullNames = tempFullNames;
+	}
+
+	function generateConfetti() {
+		setSequenceSuccess(true);
+	}
+
 	return (
-		<form onSubmit={submitHandler}>
-			<TextField
-				id="dna-text"
-				label="DNA Sequence"
-				name="text"
-				multiline
-				rows={6}
-				fullWidth
-				defaultValue="Insert a DNA sequence here..."
-				helperText={errorMessage}
-				error={isFormInvalid}
-			/>
-			<Box sx={{p: 1}}></Box>
-			<Button variant="outlined" type="submit">
-				Sequence
-			</Button>
-		</form>
+		<>
+			<form onSubmit={submitHandler}>
+				<Confetti
+					recycle={false}
+					numberOfPieces={sequenceSuccess ? 400 : 0}
+					onConfettiComplete={(confetti) => {
+						setSequenceSuccess(false);
+						if (confetti) {
+							confetti.reset();
+						}
+					}}
+				/>
+				<TextField
+					id="dna-text"
+					label="DNA Sequence"
+					name="text"
+					multiline
+					rows={6}
+					fullWidth
+					defaultValue="Insert a DNA sequence here..."
+					helperText={errorMessage}
+					error={isFormInvalid}
+					onFocus={focusHandler}
+				/>
+				<RadioGroup
+					row
+					aria-labelledby="short-or-long-aa"
+					defaultValue="short"
+					name="radio-buttons-group"
+					value={selectedValue}
+					onChange={handleChange}
+				>
+					<FormControlLabel value="short" control={<Radio />} label="Short Amino Acid Names" />
+					<FormControlLabel value="long" control={<Radio />} label="Long Amino Acid Names" />
+				</RadioGroup>
+				<Box sx={{p: 1}}></Box>
+				<Button variant="outlined" type="submit">
+					Sequence
+				</Button>
+				<Box sx={{p: 1}}></Box>
+
+				<TextField
+					id="dna-sequenced"
+					label="Polypeptides Sequenced"
+					name="dna-sequenced"
+					multiline
+					rows={6}
+					fullWidth
+					value={sequencedProtein}
+					inputProps={{readOnly: true}}
+					color="primary"
+				/>
+			</form>
+		</>
 	);
 };
 
