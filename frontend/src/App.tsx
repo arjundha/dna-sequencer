d239e30f026b1df03cccd752e2e55e6bd8f78973
Arjun <59514930+arjundha@users.noreply.github.com>
2024-06-28 17:50:41 -0700

    Deleted commented out code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,9 +3,6 @@ import FormTabs from "./components/FormTabs";
 import {Container} from "@mui/material";
 
 function App() {
-	// const [count, setCount] = useState(0);
-	// <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-
 	return (
 		<Container>
 			<h1>DNA Sequencer</h1>
