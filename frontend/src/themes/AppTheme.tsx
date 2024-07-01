bfc6489cdd6d65b92cda6124c3a586cf3613b6cf
Arjun <59514930+arjundha@users.noreply.github.com>
2024-07-01 13:50:54 -0700

    Attempt 2

--- frontend/src/themes/AppTheme.tsx
+++ frontend/src/themes/AppTheme.tsx
@@ -1,6 +1,5 @@
 import {createTheme} from "@mui/material/styles";
 import {ThemeOptions} from "@mui/material/styles";
-import Lato from "../assets/Lato-Regular.ttf";
 
 export const themeOptions: ThemeOptions = {
 	palette: {
@@ -16,7 +15,7 @@ export const themeOptions: ThemeOptions = {
 		},
 	},
 	typography: {
-		fontFamily: "Lato",
+		fontFamily: "Lato, Roboto",
 	},
 	components: {
 		MuiButton: {
@@ -31,18 +30,6 @@ export const themeOptions: ThemeOptions = {
 					padding: "0 30px",
 				},
 			},
-		},
-		MuiCssBaseline: {
-			styleOverrides: `
-			  @font-face {
-				font-family: 'Lato';
-				font-style: normal;
-				font-display: swap;
-				font-weight: 400;
-				src: local('Lato'), local('Lato-Regular'), url(${Lato}) format('ttf');
-				unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
-			  }
-			`,
 		},
 	},
 };
